f00681820f6511439ebe57520c1163d97abeb20c
Himanshu77011 <159694882+Himanshu77011@users.noreply.github.com>
2025-03-25 10:38:16 +0530

    Add files via upload
    DATE- 25/03/2025

---
+++ pybasic.py
@@ -0,0 +1,108 @@
+# print("Hello World")\
+# _____________________________________________________________________________
+# name = "Aice"
+# age = 25
+# height = 5.6
+# is_student = True
+# print(name,age,height,is_student)
+# _____________________________________________________________________________
+# name = input("Enter Your Name:")
+# print("Hello, " + name + "!")
+# _____________________________________________________________________________
+# num=int(input("Enter a number:"))
+# if num > 0:
+#     print("Positive number")
+# elif num<0:
+#     print("Negaive Number")
+# else:
+#     print("Zero")
+# _____________________________________________________________________________
+# for i in range (5):
+#     print(i)
+# _____________________________________________________________________________
+# count = 0
+# while count < 5
+# print(count)
+# count += 1
+# _____________________________________________________________________________
+# def greet(name):
+#     return "Hello" + name + "!"
+# print(greet("Alice"))
+# _____________________________________________________________________________
+# person = {"name":"Alice", "Age":25, "City":"New York"}
+# print(person["name"])
+# _____________________________________________________________________________
+# class Person:
+#     def __init__(self, name, age):  
+#         self.name = name
+#         self.age = age
+#     def greet(self): 
+#         return f"Hello, my name is {self.name} and I am {self.age} years old."
+#     p = Person("Alice", 25)     
+#     print(p.greet())  
+# _____________________________________________________________________________
+# def calculator():
+#     num1 = float(input("Enter first number: "))
+#     operator = input("Enter an operator (+, -, /, *): ")
+#     num2 = float(input("Enter second number: "))
+
+#     if operator == '+':
+#         print("Result:", num1 + num2)
+#     elif operator == '-':
+#         print("Result:", num1 - num2)
+#     elif operator == '*':
+#         print("Result:", num1 * num2)
+#     elif operator == '/':
+#         if num2 != 0:  
+#             print("Result:", num1 / num2)
+#         else:
+#             print("Cannot divide by zero.")
+#     else:
+#         print("Invalid operator.")
+#     calculator()
+# _____________________________________________________________________________
+# tasks=[]
+# def add_task(task):
+#     tasks.append(task)
+#     def remove_task(task):
+#         if task in tasks:
+#             tasks.remove(task)
+# _________________________________________________________________________________
+            
+            
+# rows = int(input("Enter number of rows: "))
+
+# k = 0
+
+# for i in range(1, rows+1):
+#     for space in range(1, (rows-i)+1):
+#         print(end="  ")
+   
+#     while k!=(2*i-1):
+#         print("* ", end="")
+#         k += 1
+   
+#     k = 0
+#     print()
+    
+# ____________________________________________________________________
+    
+for i in range(1, 6):
+    for j in range(i):
+        print('*', end='') 
+    print()
+    
+    
+# _____________________________________________________________________
+
+
+
+
+
+
+
+
+            
+            
+            
+
